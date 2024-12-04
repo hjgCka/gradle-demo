6016f64a51e48d3e87dcc59a37e15de65582c6b6
ccka <computer_cka@163.com>
2024-12-04 21:36:24 +0800

    提交代码

--- web-app/src/main/java/com/hjg/gradle/controller/HelloController.java
+++ web-app/src/main/java/com/hjg/gradle/controller/HelloController.java
@@ -1,7 +1,28 @@
-package com.hjg.gradle.controller;/**
- *  @Description
- *  @Author hjg
- *  @Date 2024-12-04 21:07
+package com.hjg.gradle.controller;
+
+import com.hjg.school.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2024-12-04 21:07
  */
+@RestController
 public class HelloController {
+
+    @RequestMapping(value = "/findByName", method = RequestMethod.GET)
+    public Student findStuByName(@RequestParam String name) {
+        Student stu = new Student();
+        stu.setName(name);
+        stu.setAge(20);
+        stu.setBirthday(new Date());
+
+        return stu;
+    }
 }
