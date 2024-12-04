dc97250688afee87819efa2df939ec1e6df6d4cb
ccka <computer_cka@163.com>
2024-12-04 21:35:34 +0800

    添加新模块，加入了controller

---
+++ web-app/src/main/java/com/hjg/gradle/WebApp.java
@@ -0,0 +1,18 @@
+package com.hjg.gradle;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2024-12-04 21:07
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
---
+++ web-app/src/main/java/com/hjg/gradle/controller/HelloController.java
@@ -0,0 +1,7 @@
+package com.hjg.gradle.controller;/**
+ *  @Description
+ *  @Author hjg
+ *  @Date 2024-12-04 21:07
+ */
+public class HelloController {
+}
