2cc585d25e00a173c1f746d5d21bbe462186b27f
ccka <computer_cka@163.com>
2024-12-24 02:15:10 +0800

    添加includeBuild和include相关模块。

---
+++ includedBuildLibs/carEntity/src/main/java/com/hjg/car/Truck.java
@@ -0,0 +1,13 @@
+package com.hjg.car;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2024-12-24 2:04
+ */
+@Data
+public class Truck {
+    private String name;
+}
---
+++ includedBuildLibs/trainEntity/src/main/java/com/hjg/train/HighWayTrain.java
@@ -0,0 +1,13 @@
+package com.hjg.train;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2024-12-24 2:05
+ */
+@Data
+public class HighWayTrain {
+    private String name;
+}
