e4a9fb1c8cf1240484249206728c805b0327f05f
ccka <computer_cka@163.com>
2025-01-27 02:24:49 +0800

    调整格式。

---
+++ stream-demo/src/main/java/com/hjg/stream/Test1.java
@@ -0,0 +1,13 @@
+package com.hjg.stream;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2025-01-27 0:16
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ stream-demo/src/main/java/com/hjg/stream/Test3.java
@@ -0,0 +1,13 @@
+package com.hjg.stream;
+
+/**
+ * @Description
+ * @Author hjg
+ * @Date 2025-01-27 0:17
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        System.out.println("world");
+    }
+}
